ecf8b7cac067bdd2da68a795ba0c01a3d74c41e8
therohitmalakar <rohitmalakar437@gmail.com>
2025-09-01 14:51:17 +0530

    ADDED HEADER

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // ✅ allow headers
+
 
   })
 );
