266375265fff08c00d316c8b6f62ac50a0496206
therohitmalakar <rohitmalakar437@gmail.com>
2025-09-02 14:55:24 +0530

    changes in cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://bite-rush-rosy.vercel.app",
-  "https://bite-rush-backend-git-main-therohitmalakars-projects.vercel.app"
+  // "https://bite-rush-backend-git-main-therohitmalakars-projects.vercel.app"
 ];
 
 app.use(
