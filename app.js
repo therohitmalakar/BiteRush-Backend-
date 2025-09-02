dda73818318608e0b109dd6f4bac8d0f581dc071
therohitmalakar <rohitmalakar437@gmail.com>
2025-09-02 15:42:02 +0530

    app.js

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"], // ✅ allow headers
+        allowedHeaders: ["Content-Type", "Authorization"], 
+        preflightContinue: false,   
+        optionsSuccessStatus: 200,
 
 
   })
